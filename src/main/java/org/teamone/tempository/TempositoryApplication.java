579a26ea4ca39defaec41707ff630d65cd2a61c6
Leegiyeon <rldus3512@naver.com>
2024-02-26 17:09:14 +0900

    Tempository 프로젝트 생성

---
+++ src/main/java/org/teamone/tempository/TempositoryApplication.java
@@ -0,0 +1,13 @@
+package org.teamone.tempository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempositoryApplication.class, args);
+    }
+
+}
