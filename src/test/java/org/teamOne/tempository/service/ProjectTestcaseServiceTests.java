be09ebf7ef77d0d9bfa555ef66cc89c7dbf3d420
oneye <onelee521@gmail.com>
2024-02-27 20:53:53 +0900

    feat(ProjectTestcase): Mapper 쿼리문 수정
    SELECT 들여쓰기 추가

--- src/test/java/org/teamOne/tempository/service/ProjectTestcaseServiceTests.java
+++ src/test/java/org/teamOne/tempository/service/ProjectTestcaseServiceTests.java
@@ -1,11 +1,8 @@
 package org.teamOne.tempository.service;
 
-import jdk.jfr.Name;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.teamOne.tempository.aggregate.ProjectTestcase;
