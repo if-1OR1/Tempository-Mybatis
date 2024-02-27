e6f97dbc60c1dfde6257fd74f1cd8582100aa131
moomint8 <office05053@gmail.com>
2024-02-27 18:36:09 +0900

    feat(User): 회원 ID 이용해 참여중인 프로젝트들 조회 기능 구현
    현재 프로젝트 관련 기능이 구현되지 않아 우선 프로젝트id와 position 만 반환하도록 구현하였습니다.
    추후 프로젝트의 제목까지 반환하도록 개선할 예정입니다.

--- src/main/java/org/teamOne/tempository/service/UserService.java
+++ src/main/java/org/teamOne/tempository/service/UserService.java
@@ -3,22 +3,38 @@ package org.teamOne.tempository.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.teamOne.tempository.aggregate.ProjectMember;
 import org.teamOne.tempository.aggregate.User;
+import org.teamOne.tempository.dao.ProjectMemberMapper;
 import org.teamOne.tempository.dao.UserMapper;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserMapper userMapper;
+    private final ProjectMemberMapper projectMemberMapper;
 
     @Autowired
-    public UserService(UserMapper userMapper) {
+    public UserService(UserMapper userMapper, ProjectMemberMapper projectMemberMapper) {
         this.userMapper = userMapper;
+        this.projectMemberMapper = projectMemberMapper;
     }
 
+    /* 설명. UserId로 사용자 정보 조회 */
     @Transactional
     public User findUserInfoById(int id) {
         User foundUserInfo = userMapper.getUserInfoById(id);
 
         return foundUserInfo;
+    }
+
+    /* 설명. 해당 유저가 참여한 모든 프로젝트 조회 */
+    @Transactional
+    public List<ProjectMember> findProjectsByUserId(int id) {
+        List<ProjectMember> joinProjects = projectMemberMapper.findProjectsByUserId(id);
+        joinProjects.forEach(System.out::println);
+
+        return joinProjects;
     }
 }
--- src/test/java/org/teamOne/tempository/service/UserServiceTests.java
+++ src/test/java/org/teamOne/tempository/service/UserServiceTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.teamOne.tempository.aggregate.ProjectMember;
 import org.teamOne.tempository.aggregate.User;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,5 +26,13 @@ class UserServiceTests {
         System.out.println("foundUserInfo = " + foundUserInfo);
 
         assertEquals(foundUserInfo.getName(), "김유진");
+    }
+
+    @DisplayName("유저가 참여중인 프로젝트 조회 테스트")
+    @Test
+    void findProjectsByUserIdTest() {
+        List<ProjectMember> foundProjects = userService.findProjectsByUserId(1);
+
+        assertNotNull(foundProjects);
     }
 }
