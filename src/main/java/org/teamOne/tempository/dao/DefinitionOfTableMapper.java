c797bdabb4f74a376f8a82104b9613ebda5e3c7c
Lee Giyeon <rldus3512@naver.com>
2024-02-27 19:51:34 +0900

    feat(DefineOfTable): 프로젝트 ID에 해당하는 테이블 정의서 조회

--- src/main/java/org/teamOne/tempository/dao/DefinitionOfTableMapper.java
+++ src/main/java/org/teamOne/tempository/dao/DefinitionOfTableMapper.java
@@ -2,13 +2,10 @@ package org.teamOne.tempository.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.teamOne.tempository.aggregate.DefinitionOfTable;
-import org.teamOne.tempository.dto.DefinitionOfTableDTO;
 
 import java.util.List;
 
 @Mapper
 public interface DefinitionOfTableMapper {
-    DefinitionOfTable isDefinitionOfTable(DefinitionOfTable definitionOfTable);
-
-    List<DefinitionOfTableDTO> selectAllDefinitionOfTables(int projectID);
+    List<DefinitionOfTable> selectAllDefinitionOfTables(DefinitionOfTable definitionOfTable);
 }
--- src/test/java/org/teamOne/tempository/service/DefinitionOfTableServiceTest.java
+++ src/test/java/org/teamOne/tempository/service/DefinitionOfTableServiceTest.java
@@ -1,44 +1,25 @@
 package org.teamOne.tempository.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.teamOne.tempository.aggregate.DefinitionOfTable;
 import org.teamOne.tempository.dto.DefinitionOfTableDTO;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DefinitionOfTableServiceTest {
 
     @Autowired
-    private DefinitionOfTableService definitionOfTable;
+    private DefinitionOfTableService definitionOfTableService;
 
-    @DisplayName("프로젝트 ID로 테이블 정의서가 존재하는지 확인")
+    @DisplayName("프로젝트 ID에 해당하는 테이블 정의서 조회")
     @Test
-    void isDefinitionOfTable() {
-        DefinitionOfTableDTO definition = new DefinitionOfTableDTO(1);
-        System.out.println("definition = " + definition);
-
-        Assertions.assertNotNull(1);
-    }
-
-
-    @DisplayName("프로젝트 ID로 테이블 정의서 모두 조회")
-    @ParameterizedTest
-    @ValueSource(ints = 1)
-    void findAllDefinitionOfTables(int projectID) {
-        Assertions.assertDoesNotThrow(
-                () -> {
-                    List<DefinitionOfTableDTO> definitionOfTableList = definitionOfTable.findAllDefinitionOfTables(projectID);
-                    definitionOfTableList.forEach(System.out::println);
-                }
-        );
-
+    void selectAllDefinitionOfTables() {
+        DefinitionOfTableDTO definitionDTO = new DefinitionOfTableDTO(1);
+        List<DefinitionOfTable> selectAllDefinition = definitionOfTableService.selectAllDefinitionOfTables(definitionDTO);
+        selectAllDefinition.forEach(System.out::println);
     }
 }
